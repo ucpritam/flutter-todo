13a809aded196a909d22f0effda931e4438ca5f6
s4njeev <s4njeev.av@gmail.com>
2021-06-29 22:38:27 +0530

    Created ToDo list Screen|Flutter

---
+++ lib/Pages/todoListPage.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class ToDoHome extends StatefulWidget {
+  @override
+  _ToDoHomeState createState() => _ToDoHomeState();
+}
+
+class _ToDoHomeState extends State<ToDoHome> {
+  Widget _buildTasks(int index) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 25),
+      child: VStack([
+        ListTile(
+          title: "ToDo Title".text.make(),
+          subtitle: "july 5, 2021 • High".text.make(),
+          trailing: Checkbox(
+            onChanged: (value) {
+              print(value);
+            },
+            activeColor: Theme.of(context).primaryColor,
+            value: true,
+          ),
+        ),
+        Divider(
+          color: Theme.of(context).primaryColor,
+        )
+      ]),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 30,
+        ),
+        backgroundColor: Theme.of(context).accentColor,
+        onPressed: () {
+          "Tapped".text.make();
+        },
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.symmetric(vertical: 60),
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) {
+          if (index == 0) {
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25, vertical: 25 / 2),
+              child: VStack(
+                [
+                  "Thinks To Do".text.black.size(40).make(),
+                  SizedBox(height: 10),
+                  ""
+                      .text
+                      .gray700
+                      .size(20)
+                      .fontWeight(FontWeight.w600)
+                      .make(),
+                ],
+                crossAlignment: CrossAxisAlignment.start,
+              ),
+            );
+          }
+          return _buildTasks(index);
+        },
+      ),
+    );
+  }
+}
